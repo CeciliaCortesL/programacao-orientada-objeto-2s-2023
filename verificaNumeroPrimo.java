21e64469274d42e04c05c940993f0fc4e8fe779d
Cecilia Cortes Lopes <105022346+CeciliaCortesL@users.noreply.github.com>
2023-08-11 10:41:50 -0300

    Add files via upload

---
+++ verificaNumeroPrimo.java
@@ -0,0 +1,11 @@
+package atividade1;
+
+/**
+ * verificaNumeroPrimo
+ */
+public class verificaNumeroPrimo {
+
+    public static void main(String args[]){
+        System.out.println("Ola, mundo");
+    }
+}
